71de45f7ae9f44ab28f1c2a7b7bff31a27ec9a71
SpitfireGG <raambasnet02@gmail.com>
2025-09-26 18:22:33 +0545

    flyekks

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Kenzo's personal flake";
+  description = "archbishop personal flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,45 +7,54 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    ags.url = "github:ozwaldorf/ags";
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    astal.url = "github:aylur/astal";
+
+    ags.url = "github:aylur/ags";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      hm,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations = {
-        kenzo = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/dell/configuration.nix
-            ./modules/nixos/ciscoPacketTracer/packetTracerByp.nix
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    hm,
+    stylix,
+    nixvim,
+    ...
+  } @ inputs: {
+    nixosConfigurations = {
+      archbishop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/dell/configuration.nix
+          ./modules/packetTracerByp.nix
+        ];
       };
-      homeConfigurations = {
-        kenzo = hm.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config = {
-              allowUnfree = true;
-            };
-          };
-          extraSpecialArgs = { inherit inputs; };
-          modules = [ ./kenzo/home.nix ];
-        };
-      };
-      kenzo = self.nixosConfigurations.kenzo.config.system.build.toplevel;
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
     };
+    homeConfigurations = {
+      archbishop = hm.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
+          };
+        };
+        extraSpecialArgs = {inherit inputs;};
+        modules = [
+          ./archbishop/home.nix
+          stylix.homeModules.stylix
+          nixvim.homeManagerModules.nixvim
+        ];
+      };
+    };
+    archbishop = self.nixosConfigurations.archbishop.config.system.build.toplevel;
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+  };
 }
